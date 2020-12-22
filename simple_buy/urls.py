5e3e977875f53321482154fb890895735292d568
Your Name <you@example.com>
2020-12-22 22:36:02 +0800

    redirect to main page

--- simple_buy/urls.py
+++ simple_buy/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 from . import views as simple_buy_views
 
 app_name = 'simple_buy'
 urlpatterns = [
+    path('', RedirectView.as_view(url='/simple_buy/index')),
     path('index/', simple_buy_views.index, name='index'),
     path('orders/', simple_buy_views.orders, name='orders'),  # GET, POST
     path('orders/<int:order_id>', simple_buy_views.delete_order, name='delete_order'),  # DELETE, POST
