2caaa9deb02140b13e35084a0ff5e8fe45748717
Your Name <you@example.com>
2020-12-22 21:40:53 +0800

    DEBUG, ALLOW_HOSTS 參數移到.env

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -26,10 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]',]
-
+DEBUG = os.getenv('DEBUG') == "True"
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 # Application definition
 
 INSTALLED_APPS = [
