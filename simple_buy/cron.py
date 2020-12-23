6a6082b40d3ea88c14c16623842b2ab4b630dd11
Your Name <you@example.com>
2020-12-23 16:25:26 +0800

    cron

--- simple_buy/cron.py
+++ simple_buy/cron.py
@@ -1,9 +1,11 @@
 import json
+from datetime import date, timedelta
 from .handlers import get_shop_handler
 
 
-def my_scheduled_job(ofile):
+def my_scheduled_job(to_dir):
     data = get_shop_handler('sales')
     print(data)
-    #with open(ofile, 'w') as fp:
-    #    json.dump(fp, )
+    d = date.today() - timedelta(days=1)
+    with open(f'{to_dir}/{d.isoformat()}', 'w') as fp:
+        json.dump(fp, data)
