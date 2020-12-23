46f0f0b011ac4dab95f6ae41c6ca4be98df500b5
Your Name <you@example.com>
2020-12-23 16:41:54 +0800

    json output

--- simple_buy/cron.py
+++ simple_buy/cron.py
@@ -5,7 +5,6 @@ from .handlers import show_sales_detail
 
 def my_scheduled_job(to_dir):
     data = show_sales_detail()
-    print(data)
     d = date.today() - timedelta(days=1)
     with open(f'{to_dir}/{d.isoformat()}.json', 'w') as fp:
-        json.dump(fp, data)
+        json.dump(data, fp)
