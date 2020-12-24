a91252c193cda61a589ebf5f41b354895e575684
Your Name <you@example.com>
2020-12-24 10:20:10 +0800

    record_meta decorator

--- simple_buy/views.py
+++ simple_buy/views.py
@@ -16,7 +16,7 @@ logger.setLevel(os.environ.get("LOG_LEVEL", "INFO"))
 # Create your views here.
 def record_meta(func):
     def wrap(*args, **kwargs):
-        request = kwargs['request']
+        request = args[0]
         logger.info(f"{request.headers}, {request.META['REMOTE_ADDR']}, {request.META['REMOTE_HOST']}")
         return func(*args, **kwargs)
     return wrap
